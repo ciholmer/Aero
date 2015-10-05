bfcc398b3194bb0e98e3db39265780b4f3772d63
Curt <cholmer@ciholmer.com>
2015-10-05 19:47:52 -0400

    Part 2

--- DataService3/grails-app/conf/spring/resources.groovy
+++ DataService3/grails-app/conf/spring/resources.groovy
@@ -1,3 +1,19 @@
 // Place your Spring DSL code here
+import grails.rest.render.xml.*
+import grails.rest.render.json.*
+import com.ciholmer.rest.City
+ 
 beans = {
+    cityXmlRenderer(XmlRenderer, City) {
+        excludes = ['class', 'dateCreated']
+    }
+    cityJSONRenderer(JsonRenderer, City) {
+        excludes = ['class', 'dateCreated']
+    }
+    cityXmlCollectionRenderer(XmlCollectionRenderer, City) {
+        excludes = ['class', 'dateCreated']
+    }
+    cityJSONCollectionRenderer(JsonCollectionRenderer, City) {
+        excludes = ['class', 'dateCreated']
+    }
 }
